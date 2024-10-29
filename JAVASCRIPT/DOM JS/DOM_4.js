d263014ba8c932ab530a7c54b622cb7a15743c00
kalmatheroshan <kalmatheroshan@gmail.com>
2024-10-29 23:54:59 +0530

    group of commit

--- JAVASCRIPT/DOM JS/DOM_4.js
+++ JAVASCRIPT/DOM JS/DOM_4.js
@@ -1,9 +1,8 @@
-
-let meraPromise = new Promise(function (resolve, reject) {
-    console.log('Promise main hun .. ');
-    resolve(101);
-    // reject(new Error('Hello Roshan, How is the journey'));
-})
+// let meraPromise = new Promise(function (resolve, reject) {
+//     console.log('Promise main hun .. ');
+//     resolve(101);
+//     // reject(new Error('Hello Roshan, How is the journey'));
+// })
 
 // meraPromise.then((val) => {
 //     console.log("Promise Resolve ", val);
@@ -13,44 +12,80 @@ let meraPromise = new Promise(function (resolve, reject) {
 //     console.log('Promise Rejected => ', error);
 // })
 
-// then and catch in one line
-meraPromise.then((val) => {
-    console.log("resolve val is ", val);
-}, (error) => { console.log('error aayo .. ') })
+// // then and catch in one line
+// meraPromise.then((val) => {
+//     console.log("resolve val is ", val);
+// }, (error) => { console.log('error aayo .. ') })
 
 
 
-// ******* Promises ***********
-let promise_1 = new Promise((resolve, reject) => {
-    setTimeout(function () {
-        console.log("First Promise ... ");
-    }, 4000)
-    resolve("first resolve");
-})
+// // ******* Promises ***********
+// let promise_1 = new Promise((resolve, reject) => {
+//     setTimeout(function () {
+//         console.log("First Promise ... ");
+//     }, 4000)
+//     resolve("first resolve");
+// })
 
-let output = promise_1.then(() => {
-    let promise_2 = new Promise((resolve, reject) => {
-        setTimeout(function () {
-            console.log('Second Promise ... ');
-        }, 6000)
-        resolve('second resolve');
-    })
-})
+// let output = promise_1.then(() => {
+//     let promise_2 = new Promise((resolve, reject) => {
+//         setTimeout(function () {
+//             console.log('Second Promise ... ');
+//         }, 6000)
+//         resolve('second resolve');
+//     })
+// })
 
-console.log(output.then((val) => { console.log('value is ', val); }));
+// console.log(output.then((val) => { console.log('value is ', val); }));
 
-// how to make any code async => always return promise
-async function Hello() {
-    return "I am async code ..";
-}
-console.log('Returning to ', Hello());
-console.log("console ... ");
-
+// // how to make any code async => always return promise
+// async function Hello() {
+//     return "I am async code ..";
+// }
+// console.log('Returning to ', Hello());
+// console.log("console ... ");
 
 // async await
+//without async await
+async function utility() {
+    // paralell promise
+    let delhiWeather = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Delhi Weather ');
+        }, 8000);
+    });
 
+    let gujaratWeather = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Gujarat Weather ');
+        }, 6000);
+    });
 
+    let dW = delhiWeather;
+    let gW = gujaratWeather;
+    return [dW, gW];
+}
+
+// with async await
+async function utility_2() {
+    let delhiWeather = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Delhi Weather ');
+        }, 8000);
+    });
+
+    let gujaratWeather = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Gujarat Weather ');
+        }, 6000);
+    });
+
+    let dW = await delhiWeather;
+    let gW = await gujaratWeather;
+    return [dW, gW];
+}
 
 // api
-// fetch('url' , [options])
+// fetch('url' , [options]) for post , fetch('url') for get
+
 
