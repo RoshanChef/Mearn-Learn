4f1e9db764ac678065317ea122930b93638e07ab
kalmatheroshan <kalmatheroshan@gmail.com>
2024-12-30 01:56:46 +0530

    Authentication code added

---
+++ 8 Express/Authentication/Jwt.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const app = express();
+
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'randomroshankalmathe';
+
+// for the users array 
+app.use(express.json())
+
+// in memory database
+const users = [];
+
+function generateToken(length = 16) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let token = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        token += characters[randomIndex];
+    }
+    return token;
+}
+
+
+app.post('/signUp', function (req, res) {
+
+    let username = req.body.username;
+    let password = req.body.password;
+
+    if (users.find(ele => ele.username === username)) {
+        res.json({
+            mes: 'user already exists'
+        })
+    } else {
+        users.push({ username: username, password: password });
+        res.json({
+            mes: 'user are signIn successfully ..'
+        })
+    }
+
+});
+
+app.post('/signIn', function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let foundUser = null;
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username === username && users[i].password === password) {
+            foundUser = users[i];
+        }
+    }
+
+    if (foundUser) {
+        // encode the username
+        let token = jwt.sign({
+            username: username,
+        }, JWT_SECRET);
+        res.json({
+            token: token,
+        });
+    } else {
+        res.status(404).json({
+            message: 'Invalid username or password'
+        })
+    }
+});
+
+app.get('/me', (req, res) => {
+    const token = req.headers.token;
+    // decode the username
+    const decodeInformation = jwt.verify(token, JWT_SECRET); // {username : roshankalmthe@gmail.com}
+    const username = decodeInformation.username;
+
+    let foundUser = null;
+    // find the user
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username === username) {
+            foundUser = users[i];
+        }
+    }
+
+
+    if (foundUser) {
+        res.json({
+            username: foundUser.username,
+            password: foundUser.password
+        })
+    } else {
+        res.status(404).json({ message: 'Not found' });
+    }
+})
+
+
+app.listen(5500); 
---
+++ 8 Express/Authentication/Token.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const app = express();
+
+// for the users array 
+const users = [];
+app.use(express.json());
+
+function generateToken(length = 16) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let token = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        token += characters[randomIndex];
+    }
+    return token;
+}
+
+
+app.post('/signUp', function (req, res) {
+
+    let username = req.body.username;
+    let password = req.body.password;
+
+    if (users.find(ele => ele.username === username)) {
+        res.json({
+            mes: 'user already exists'
+        })
+    } else {
+        users.push({ username: username, password: password });
+        res.json({
+            mes: 'user are signIn successfully ..'
+        })
+    }
+
+});
+
+app.post('/signIn', function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let foundUser = null;
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username === username && users[i].password === password) {
+            foundUser = users[i];
+        }
+    }
+
+    if (foundUser) {
+        let token = generateToken();
+        foundUser.token = token;
+        res.json({
+            token: token,
+        });
+    } else {
+        res.status(404).json({
+            message: 'Invalid username or password'
+        })
+    }
+});
+
+app.get('/me', (req, res) => {
+    const token = req.headers.token;
+    let foundUser = null;
+    // find the user
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].token === token) {
+            foundUser = users[i];
+        }
+    }
+
+    if (foundUser) {
+        res.json({
+            username: foundUser.username,
+            password: foundUser.password
+        })
+    } else {
+        res.status(404).json({ message: 'Not found' });
+    }
+})
+
+
+app.listen(5500); 
