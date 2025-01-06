67c037ce792f60c8222f5d0d5b35a2c4a213a8dd
kalmatheroshan <kalmatheroshan@gmail.com>
2025-01-07 01:13:42 +0530

    'done'

--- 7 React_Project/Project 7 useMemo/src/App.jsx
+++ 7 React_Project/Project 7 useMemo/src/App.jsx
@@ -1,21 +1,34 @@
-import { useRef, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import './app.css';
 
 export default function App() {
   const [cnt, setCnt] = useState(0);
+  const [input, setInput] = useState(1);
+  const [increment, setIncrement] = useState(1);
+
 
   function expensiveTask(num) {
     for (let i = 0; i < 1000000000; i++) { }
     return num * 2;
   }
 
-  let doubleValue = expensiveTask(2);
+  let doubleValue = useMemo(() => expensiveTask(input), [input]);
+  //every Refresh
+  useEffect(() => {
+    console.log('Render Happened ... ');
+  })
 
   return (
     <div className="App">
       <button onClick={() => setCnt(cnt + 1)}>Increase</button>
       <h1>Count : {cnt}</h1>
-      <h1>DoubleValue : {doubleValue}</h1>
+      <h1>Double : {doubleValue}</h1>
+      <h3>Third : {increment}</h3>
+      <button onClick={() => { setIncrement(increment + 1) }}> Third Btn</button>
+
+      <input type="number" placeholder='Enter the number' value={input} onChange={(e) => setInput(e.target.value)} />
+
+
     </div>
   );
 }
