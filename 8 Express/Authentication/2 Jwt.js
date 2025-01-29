1b68efa09f8e9c47452aa4001b80a9ca3ee64642
kalmatheroshan <kalmatheroshan@gmail.com>
2025-01-29 10:56:14 +0530

    jwt done

--- 8 Express/Authentication/2 Jwt.js
+++ 8 Express/Authentication/2 Jwt.js
@@ -1,85 +1,73 @@
 const express = require('express');
-const app = express();
-
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = 'randomroshankalmathe';
+const app = express();
+const JWT_SECRET = 'randomxyz';
 
-// for the users array 
-app.use(express.json())
+
+
+/*
+JWT essentially prevents the 1 round trip from the backend server and DB for any user request. 
+
+library => jsonwebtoken
+ 
+step 1 : asign jwt_secret 
+step 2 : encode => jwt.sign({},key)
+step 3 : decode => jwt.verify({},key)
+*/
+
+// Middleware
+app.use(express.json());
 
 // in memory database
 const users = [];
 
 
-app.post('/signUp', function (req, res) {
-
-    let username = req.body.username;
-    let password = req.body.password;
-
-    if (users.find(ele => ele.username === username)) {
-        res.json({
-            mes: 'user already exists'
-        })
-    } else {
-        users.push({ username: username, password: password });
-        res.json({
-            mes: 'user are signIn successfully ..'
-        })
-    }
-
-});
-
-app.post('/signIn', function (req, res) {
+app.post('/signup', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    let foundUser = null;
-    for (let i = 0; i < users.length; i++) {
-        if (users[i].username === username && users[i].password === password) {
-            foundUser = users[i];
-        }
+    if (users.username === username)
+        res.json({ message: 'user already exist' });
+    else {
+        users.push({ username: username, password: password });
+        res.json({ message: 'secessfully done' });
     }
+});
 
-    if (foundUser) {
-        // encode the username
-        let token = jwt.sign({
-            username: username,
-        }, JWT_SECRET);
-        res.json({
-            token: token,
-        });
-    } else {
-        res.status(404).json({
-            message: 'Invalid username or password'
-        })
+app.post('/signin', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    //find into inmemory db
+    let findInx = users.findIndex(user => user.username === username && user.password === password);
+
+    // returning the token as jwt
+    if (findInx !== -1) {
+        // encode
+        let token = jwt.sign({ username: username }, JWT_SECRET);
+        res.json({ token: token });
+    }
+    else {
+        res.status(404).send('user not found');
     }
 });
 
 // authenticated endpoint
 app.get('/me', (req, res) => {
     const token = req.headers.token; //jwt
-    // decode the username
-    const decodeInformation = jwt.verify(token, JWT_SECRET); // {username : roshankalmthe@gmail.com}
-    const username = decodeInformation.username;
+    // decode
+    let decondedInfromation = jwt.verify(token, JWT_SECRET); // {username : roshankalmthe@gmail.com}
+    let username = decondedInfromation.username;
 
-    let foundUser = null;
-    // find the user
-    for (let i = 0; i < users.length; i++) {
-        if (users[i].username === username) {
-            foundUser = users[i];
-        }
+        // find the other data
+    let inx = users.findIndex(user => user.username === username);
+
+    if (inx !== -1) {
+        res.json(users[inx]);
     }
-
-
-    if (foundUser) {
-        res.json({
-            username: foundUser.username,
-            password: foundUser.password
-        })
-    } else {
-        res.status(404).json({ message: 'Not found' });
+    else {
+        res.status(404).json({ message: 'User not found' });
     }
 })
 
-
-app.listen(5500); 
+app.listen(80, () => console.log('server is running ..'))
