101a86a3bb1290cd1edac825ba1aba2416b17a59
kalmatheroshan <kalmatheroshan@gmail.com>
2025-01-30 16:25:58 +0530

    gsap project building

---
+++ Gsap Project 1/script.js
@@ -0,0 +1,39 @@
+function navAnimation() {
+    let nav = document.querySelector('nav');
+
+    nav.addEventListener('mouseenter', e => {
+        let tl = gsap.timeline();
+        tl.to('.nav_part3', {
+            height: '23vh',
+        })
+    
+        tl.to('.nav_part2 h5', {
+            display: 'block',
+        })
+    
+        tl.to('.nav_part2 h5 span', {
+            y: 0,
+            stagger: { amount: .6 },
+            duration: .3,
+        })
+    });
+    nav.addEventListener('mouseleave', e => {
+        let tl = gsap.timeline();
+    
+    
+        tl.to('.nav_part2 h5 span', {
+            y: 20,
+            stagger: { amount: .2 },
+        })
+        tl.to('.nav_part2 h5', {
+            display: 'none',
+            duration: .2
+        })
+        tl.to('.nav_part3', {
+            height: 0,
+            duration: .2
+        })
+    });     
+}
+
+navAnimation(); 
