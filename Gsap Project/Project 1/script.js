1923f098d894719dd9a3c59a73d01e434feedc8b
kalmatheroshan <kalmatheroshan@gmail.com>
2025-02-01 13:07:39 +0530

    gsap project added

--- Gsap Project/Project 1/script.js
+++ Gsap Project/Project 1/script.js
@@ -6,34 +6,34 @@ function navAnimation() {
         tl.to('.nav_part3', {
             height: '23vh',
         })
-    
+
         tl.to('.nav_part2 h5', {
             display: 'block',
         })
-    
+
         tl.to('.nav_part2 h5 span', {
             y: 0,
             stagger: { amount: .6 },
-            duration: .3,
+            duration: .2,
         })
     });
     nav.addEventListener('mouseleave', e => {
         let tl = gsap.timeline();
-    
-    
+
+
         tl.to('.nav_part2 h5 span', {
             y: 20,
             stagger: { amount: .2 },
         })
         tl.to('.nav_part2 h5', {
             display: 'none',
-            duration: .2
+            duration: .1
         })
         tl.to('.nav_part3', {
             height: 0,
             duration: .2
         })
-    });     
+    });
 }
 
 navAnimation(); 
---
+++ Gsap Project/Project 2/script.js
@@ -0,0 +1,22 @@
+function animation() {
+    gsap.to('.page1 #img1', {
+        opacity: '1',
+        duration: 2,
+        rotate: '0deg',
+        x: 360
+    })
+    gsap.to('.page1 #img2', {
+        rotate: '0deg',
+        opacity: '1',
+        duration: 2,
+        x: -360
+    })
+
+    gsap.from('nav h1:nth-child(1)', {
+        duration: 1,
+        x: -100,
+        rotate: '30deg'
+    })
+}
+animation(); 
+
