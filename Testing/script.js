c1f4f14973fdef2759328172f4214cd952f6a019
kalmatheroshan <kalmatheroshan@gmail.com>
2025-02-14 01:09:48 +0530

    img removed

--- Testing/script.js
+++ Testing/script.js
@@ -4,3 +4,12 @@
 // con.addEventListener('click', (e) => {
 //     con.classList.toggle('len');
 // })
+
+let arr = [{ name: 'Roshan', age: 22 }, { name: 'Rohit', age: 23 }];
+
+console.log(arr);
+
+let obj = { name: 'Mohit', age: 28 };
+arr = [...arr, obj];
+
+console.log(arr);
