ada47bc311a156a39537c5339f2822837f534f1b
kalmatheroshan <kalmatheroshan@gmail.com>
2025-03-09 20:11:31 +0530

    created slider card

--- Testing/script.js
+++ Testing/script.js
@@ -2,4 +2,7 @@ let bx = document.querySelector('.box');
 let text = document.querySelector('.text');
 text.innerHTML = text.innerText.split('').map((val, inx) =>
     `<span style="transform:rotate(${inx * 8.3}deg)">${val}</span>`
-).join(''); 
+).join('');
+
+
+
