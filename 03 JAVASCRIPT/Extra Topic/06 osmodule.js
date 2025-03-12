4b8cbb7766b185b2c820bd7fc46ae46ed908df51
kalmatheroshan <kalmatheroshan@gmail.com>
2025-03-12 17:17:40 +0530

    os module learnt

---
+++ 03 JAVASCRIPT/Extra Topic/06 osmodule.js
@@ -0,0 +1,7 @@
+const os = require('os'); 
+console.log("type " , os.type()); 
+console.log("platform " , os.platform());
+console.log("arch " , os.arch());
+console.log("cpus " , os.cpus());
+console.log("freemem " , os.freemem());
+console.log("totalmem " , os.totalmem());
