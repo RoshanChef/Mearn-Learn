b00dea00bcf58b857c020cab8dcc7cec4c801663
kalmatheroshan <kalmatheroshan@gmail.com>
2025-03-22 20:54:59 +0530

    Debounce better code

--- 03 JAVASCRIPT/Extra Topic/Debounce.js
+++ 03 JAVASCRIPT/Extra Topic/Debounce.js
@@ -1,24 +1,41 @@
-function search() {
-    console.log('Request sended ');
-    // fetch(); 
+function search(query) {
+    console.log('search for ', query);
+    // fetch call .. 
+    // fetch(url+${word})
 }
 
-let timer;
+// search('H');
+// search('Hi');
+// search('Hi ');
+// search('Hi T');
+// search('Hi Th');
+// search('Hi The');
+// search('Hi Ther');
+// search('Hi There');
+// search('Hi There!');
+// search('Hi There!!');
 
-function DebounceSerach() {
-    if (timer)
-        clearTimeout(timer);
-    timer = setTimeout(() => {
-        search();
-    }, 30);
+let timerId;
+function debounce(fnx, delay) {
+    return function (word) {
+        if (timerId) {
+            clearTimeout(timerId);
+        }
+        timerId = setTimeout(function () {
+            fnx(word);
+        }, delay);
+    }
 }
 
-input.addEventListner('change', () => {
-    DebounceSerach();
-})
+let DebounceSerach = debounce(search, 300);
 
-DebounceSerach();
-DebounceSerach();
-DebounceSerach();
-DebounceSerach();
-DebounceSerach(); 
+DebounceSerach('H');
+DebounceSerach('Hi');
+DebounceSerach('Hi ');
+DebounceSerach('Hi T');
+DebounceSerach('Hi Th');
+DebounceSerach('Hi The');
+DebounceSerach('Hi Ther');
+DebounceSerach('Hi There');
+DebounceSerach('Hi There!');
+DebounceSerach('Hi There!!');
