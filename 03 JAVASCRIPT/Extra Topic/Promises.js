39101c58b5b56625c48f30892a4bf61424888af5
kalmatheroshan <kalmatheroshan@gmail.com>
2025-05-12 11:39:51 +0530

    changes are maded

--- 03 JAVASCRIPT/Extra Topic/Promises.js
+++ 03 JAVASCRIPT/Extra Topic/Promises.js
@@ -11,16 +11,29 @@ let promise2 = new Promise((resolve, reject) => {
 })
 
 /*  
+ Promise.all()
+    Waits for all promises to fulfill.
+    Fails fast: if any promise rejects, it rejects immediately.
+*/
+// Promise.all([promise1, promise2]).then(val => console.log(val)).catch(err => console.log(err));
+
+/*  
 Promise.any(promises[])
     Returns the first fulfilled promise
     Ignores rejections
     If all promises reject, it throws AggregateError
 */
-// Promise.all([promise1, promise2]).then(val => console.log(val)).catch(err => console.log(err));
-
 // Promise.any([promise1, promise2]).then(val => console.log(val)).catch(err => console.log(err));
-// Promise.allSettled([promise1, promise2]).then(val => console.log(val));
+
 // Promise.race([promise1, promise2]).then(val => console.log(val)).catch(err => console.log(err));
+
+/*
+Promise.allSettled()
+Waits for all promises to settle (either resolve or reject).
+Never fails early.
+no need of catch
+*/
+// Promise.allSettled([promise1, promise2]).then(val => console.log(val));
 
 
 console.log('I am still working fine'); 
